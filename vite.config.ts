7bc4623b19f63076457a84360d3fc011f795d756
jxvrmsqt <jessicaxaviermesquita@gmail.com>
2026-05-06 16:00:30 +0000

    white page

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
   },
   server: {
     port: 3000,
